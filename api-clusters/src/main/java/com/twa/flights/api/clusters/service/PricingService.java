f31e3da326f4606ed1f9217351ee75c9246f5141
cristianregnier <cristianregnier@gmail.com>
2022-04-09 11:30:38 -0300

    Mark circuit breaker in required methods

--- api-clusters/src/main/java/com/twa/flights/api/clusters/service/PricingService.java
+++ api-clusters/src/main/java/com/twa/flights/api/clusters/service/PricingService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,7 @@ public class PricingService {
         this.pricingConnector = pricingConnector;
     }
 
+    @CircuitBreaker(name = "priceItineraries")
     public List<ItineraryDTO> priceItineraries(List<ItineraryDTO> itineraries) {
         LOGGER.debug("Pricing itineraries");
 
