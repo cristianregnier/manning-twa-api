37b328821f86a92f0bdbdbf4d04faf9fa5ca272e
cristianregnier <cristianregnier@gmail.com>
2022-04-10 11:20:33 -0300

    Add rate limit to availability method

--- api-itineraries-search/src/main/java/com/twa/flights/api/itineraries/search/facade/ProviderAlphaFacade.java
+++ api-itineraries-search/src/main/java/com/twa/flights/api/itineraries/search/facade/ProviderAlphaFacade.java
@@ -3,6 +3,7 @@ package com.twa.flights.api.itineraries.search.facade;
 import java.util.List;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,7 @@ public class ProviderAlphaFacade implements ProviderFacade {
     }
 
     @CircuitBreaker(name = "providerAlpha")
+    @RateLimiter(name = "providerAlpha")
     public List<ItineraryDTO> availability(AvailabilityRequestDTO request) {
         LOGGER.debug("Obtain the information about the flights");
         return itinerariesSearchConnector.availability(request);
